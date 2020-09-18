3766bc070db3b5b57062b89082dc5926fda01a2e
zh69ou <zhou69.1@qq.com>
2020-09-18 18:17:47 +0800

    路由配置

--- src/config/router.js
+++ src/config/router.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
-import menu from './menu.js'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import routes from './menu.js'
 
-const router = new Router({
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+const originalreplace = VueRouter.prototype.replace
+VueRouter.prototype.replace = function replace(location) {
+  return originalreplace.call(this, location).catch(err => err)
+}
+const router = new VueRouter({
 	// mode: 'history',
-	routes:menu,
+	routes,
 })
 
 router.beforeEach((to, from, next) => {
-	// console.log(to)
+	next()
 })
 
 
