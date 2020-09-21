a8f0d7085d885a8c256df0128a61840ee78582b4
zh69ou <zhou69.1@qq.com>
2020-09-21 15:20:15 +0800

    图片打包配置调整

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
 			{
 				test: /\.(png|jpeg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
 				use:[{
-					loader:'url-loader',
+					loader:'file-loader',
 					options:{
 						name:'[name].[ext]',
 						outputPath:config.AssetsDir+'/img/'
